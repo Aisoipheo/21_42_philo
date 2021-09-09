3fdaa183008631841441720ca3cfa934742de64c
Rafe Drizzle <rdrizzle@oa-i5.msk.21-school.ru>
2021-09-09 17:11:39 +0300

    prep done i think

--- philo/src/jobs.c
+++ philo/src/jobs.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   jobs.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdrizzle <rdrizzle@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/04 16:38:18 by rdrizzle          #+#    #+#             */
-/*   Updated: 2021/09/09 16:29:00 by rdrizzle         ###   ########.fr       */
+/*   Created: 2021/09/09 16:02:54 by rdrizzle          #+#    #+#             */
+/*   Updated: 2021/09/09 16:02:58 by rdrizzle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
-
-# include <pthread.h>
-
-typedef struct s_args
-{
-	int		nphilo;
-	int		dtime;
-	int		etime;
-	int		stime;
-	int		neat;
-}			t_args;
-
-typedef struct s_philo_arg
-{
-	int					id;
-	pthread_mutex_t		*mutexes;
-	t_args				*args;
-}		t_philo_arg;
-
-void	*philo_job(void *arg);
-
-#endif
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: rdrizzle <rdrizzle@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 15:16:05 by rdrizzle          #+#    #+#             */
-/*   Updated: 2021/09/09 11:03:18 by rdrizzle         ###   ########.fr       */
+/*   Updated: 2021/09/09 16:32:32 by rdrizzle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 static void	parse_argv(t_args *args, int argc, char *argv[])
 {
@@ -29,23 +30,62 @@ static void	parse_argv(t_args *args, int argc, char *argv[])
 		args->neat = -1;
 }
 
-static void	simulate(t_args *args)
+static void	init(pthread_t **t, pthread_mutex_t **m,
+				t_args *args, t_philo_arg **p)
 {
-	printf("nphilo: %d\n", args->nphilo);
-	printf("dtime: %d\n", args->dtime);
-	printf("etime: %d\n", args->etime);
-	printf("stime: %d\n", args->stime);
-	printf("neat: %d\n", args->neat);
+	int		i;
+
+	*t = (pthread_t *)malloc(sizeof(pthread_t) * args->nphilo);
+	*m = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * args->nphilo);
+	*p = (t_philo_arg *)malloc(sizeof(t_philo_arg) * args->nphilo);
+	i = 0;
+	while (i < args->nphilo)
+	{
+		pthread_mutex_init(&(*m)[i], NULL);
+		(*p)[i].id = i;
+		(*p)[i].mutexes = *m;
+		(*p)[i].args = args;
+	}
+}
+
+static void	simulate(pthread_t **t, t_args *args, t_philo_arg **p)
+{
+	int		i;
+
+	i = 0;
+	while (i < args->nphilo)
+		pthread_create(&(*t[i]), NULL, philo_job, &(*p)[i]);
+}
+
+static void	destroy(pthread_t **t, pthread_mutex_t **m,
+					t_args *args, t_philo_arg **p)
+{
+	int		i;
+
+	i = 0;
+	while (i < args->nphilo)
+	{
+		pthread_join(&(*t)[i], NULL);
+		pthread_mutex_destroy(&(*m)[i]);
+		++i;
+	}
+	free(*t);
+	free(*m);
+	free(*p);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_args	args;
+	t_args				args;
+	pthread_t			*threads;
+	pthread_mutex_t		*mutexes;
+	t_philo_arg			*philo_args;
 
 	if (argc != 5 && argc != 6)
 		return (1);
-	go_sleep(1000);
 	parse_argv(&args, argc, argv);
-	simulate(&args);
+	init(&threads, &mutexes, &args, &philo_args);
+	simulate(&threads, &args, &philo_args);
+	destroy(&threads, &mutexes, &args, &philo_args);
 	return (0);
 }
