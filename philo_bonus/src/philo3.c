98493b9aecbbd520598ee15a74fcd0ba7d4c09c1
Rafe Drizzle <rdrizzle@oa-h5.msk.21-school.ru>
2021-09-30 12:21:14 +0300

    philo_bonus

---
+++ philo_bonus/src/philo3.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo3.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdrizzle <rdrizzle@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 10:40:30 by rdrizzle          #+#    #+#             */
+/*   Updated: 2021/09/30 12:07:46 by rdrizzle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include "utils.h"
+
+#include <stdio.h>
+
+void	print_msg(t_worker *w, const char *restrict msg)
+{
+	sem_wait(w->global->printer);
+	printf("%llu %d %s\n", get_unix_time() - w->global->start, w->id + 1, msg);
+	sem_post(w->global->printer);
+}
+
+void	sleep_think(t_worker *w)
+{
+	if (w->is_game)
+		print_msg(w, "is sleeping");
+	go_sleep(w->global->stime);
+	if (w->is_game)
+		print_msg(w, "is thinking");
+}
+
+void	eat(t_worker *w)
+{
+	sem_wait(w->deathlock);
+	w->last_meal = get_unix_time();
+	++w->meals;
+	sem_post(w->deathlock);
+	if (w->is_game)
+		print_msg(w, "is eating");
+	go_sleep(w->global->etime);
+}
---
+++ philo_bonus/src/utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdrizzle <rdrizzle@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/04 16:56:19 by rdrizzle          #+#    #+#             */
+/*   Updated: 2021/09/22 17:23:13 by rdrizzle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+size_t	my_strlen(const char *s)
+{
+	size_t		size;
+
+	size = 0;
+	while (s && s[size])
+		++size;
+	return (size);
+}
+
+int	is_num(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	my_atoi(const char *s, int *nb)
+{
+	char	sign;
+
+	*nb = 0;
+	sign = 0;
+	while (s && *s && *s != '+' && *s != '-' && !is_num(*s))
+		s++;
+	if (*s == '-')
+		sign = 1;
+	if (*s == '+' || *s == '-')
+		s++;
+	while (s && *s && is_num(*s))
+	{
+		*nb = *nb * 10 + *s - '0';
+		s++;
+	}
+	if (sign)
+		*nb = -(*nb);
+	return (0);
+}
+
+// 1 ms = 1000 us
+void	go_sleep(unsigned long long int ms)
+{
+	unsigned long long int	ts;
+
+	ts = get_unix_time();
+	while (get_unix_time() - ts < ms)
+		usleep(50);
+}
+
+unsigned long long int	get_unix_time(void)
+{
+	static struct timeval	te;
+
+	gettimeofday(&te, NULL);
+	return (te.tv_sec * 1000ULL + te.tv_usec / 1000);
+}
